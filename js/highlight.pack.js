341699012172257e51333342135c8d237b613a0d
Green_Chennai <luvse@foxmail.com>
2018-12-19 12:20:38 +0800

    调整

--- js/highlight.pack.js
+++ js/highlight.pack.js
@@ -12,7 +12,7 @@
             span = document.createElement('span');
         span.textContent = coreSocialistValues[index];
         index = (index + 1) % coreSocialistValues.length;
-        span.style.cssText = ['z-index: 9999999; position: absolute; font-weight: bold; color: #ff6651; top: ', y - 20, 'px; left: ', x, 'px;'].join('');
+        span.style.cssText = ['z-index: 9999999; position: absolute; font-weight: bold; color:#90EE90; top: ', y - 20, 'px; left: ', x, 'px;'].join('');
         document.body.appendChild(span);
         animate(span);
     });
